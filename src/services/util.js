45b341617e3f0978ae58171642809d4cfd2413e4
BuildTools <unconfigured@null.spigotmc.org>
2020-03-17 10:39:59 -0400

    Initial Check-in

--- src/services/util.js
+++ src/services/util.js
@@ -7,6 +7,6 @@ export const formatPrice = (x, currency) => {
   }
 };
 
-export const productsAPI =
-  'https://react-shopping-cart-67954.firebaseio.com/products.json';
+export const productsAPI = 'products.json'
+ // 'https://react-shopping-cart-67954.firebaseio.com/products.json';
 // export const productsAPI = "http://localhost:8001/api/products";
